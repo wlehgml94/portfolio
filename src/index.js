4148f624dab3862df66823b21ba21b84ddb3b7be
dohee <wlehgml94@naver.com>
2023-07-03 18:30:37 +0900

    230703 :: 리액트 강의

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
+import { Fragment } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const title = "가위바위보";
+root.render(<>
+    <h1 id="title">{title}</h1>
+    <button className="hand">가위</button>
+    <button className="hand">바위</button>
+    <button className="hand">보</button>
+    </>);
